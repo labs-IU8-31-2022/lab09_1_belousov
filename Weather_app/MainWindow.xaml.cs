748dafc39ad531369adfe1ea9b964da701224fd6
the-blind-pew <maf10aaa03v00@gmail.com>
2022-12-03 03:39:13 +0300

    Lab is done...

---
+++ Weather_app/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WeatherBot;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Markup;
+
+
+namespace Weather
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-IN");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-IN");
+            LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
+                XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+            Cities = new List<City>();
+            DataContext = this;
+            InitializeCityList();
+        }
+
+        public List<City> Cities { get; set; }
+
+        public City SelectedCity { get; set; }
+
+
+        private async void InitializeCityList()
+        {
+            var path = $"{Environment.CurrentDirectory}/../../../city.txt";
+            var fileText = await File.ReadAllTextAsync(path);
+            foreach (var line in fileText.Split('\n'))
+            {
+                var city = string.Concat(line.TakeWhile(c => c is < '0' or > '9')).Trim('-').Trim();
+                var lineT = line.Replace(city, string.Empty);
+                var data = string.Concat(lineT.Where(c => c is >= '0' and <= '9' or ',' or '.' or '-')).Split(',');
+                var (lat, lon) = (Convert.ToDecimal(data[0]), Convert.ToDecimal(data[1]));
+                Cities.Add(new City(city, lat, lon));
+            }
+        }
+
+        private void GetApi(object sender, RoutedEventArgs e)
+        {
+            if (SelectedCity.Name is null)
+            {
+                ComboBox1.Text = "Город выбираем...";
+                return;
+            }
+
+            Button1.IsEnabled = false;
+            Text1.Text = "Получаем данные...";
+            var res = Task.Run(() => WeatherBot.Weather.GetAsync(SelectedCity.Lat, SelectedCity.Lon).Result);
+            res.ContinueWith((t) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Text1.Text = $"Country: {t.Result.Country}   City: {t.Result.Name} " +
+                                 $"\nWeather: {t.Result.Description} \nTemperature: {t.Result.Temp}°C";
+                    Button1.IsEnabled = true;
+                });
+            });
+        }
+
+        public struct City
+        {
+            public string Name { get; }
+            public decimal Lat { get; }
+            public decimal Lon { get; }
+
+            public City(string city, decimal lat, decimal lon) => (Name, Lat, Lon) = (city, lat, lon);
+        }
+    }
+}
---
+++ Weather_app/Weather.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherBot;
+
+internal struct Weather
+{
+    private static readonly string? ApiKey = "d37806bbb02cecdb04dabf01fdfa2975";
+    public string? Country { get; }
+    public string? Name { get; }
+    public decimal Temp { get; }
+    public string Description { get; }
+
+    private Weather(string country, string name, decimal temp, string description) =>
+        (Country, Name, Temp, Description) = (country, name, temp, description);
+
+    public static async Task<Weather> GetAsync(decimal lat, decimal lon)
+    {
+        if (lat < -90 && lat > 90 && lon < -180 && lon > 180)
+        {
+            throw new Exception("Check range of coordinates");
+        }
+
+        HttpClient client = new();
+        client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/weather");
+        var response =
+            await client.GetAsync($"?lat={lat}&lon={lon}&appid={ApiKey}&units=metric").ConfigureAwait(false);
+        //Console.WriteLine(response.EnsureSuccessStatusCode());
+        var (weather, code) = Convert.ToInt32(response.StatusCode) == 200
+            ? FromJsonDeserializer(await response.Content.ReadAsStringAsync())
+            : (new Weather(), Convert.ToInt32(response.StatusCode));
+
+        if (code == 200 && weather.Country is not null && weather.Name is not null)
+        {
+            return weather;
+        }
+
+        if (weather.Country is null || weather.Name is null)
+        {
+            throw new DataException("Null country or city");
+        }
+
+        throw new Exception("Странная поломка, да?)");
+    }
+
+    private static (Weather w, int code) FromJsonDeserializer(string json)
+    {
+        var dynamic = JsonConvert.DeserializeObject<dynamic>(json);
+
+        var country = Convert.ToString(dynamic?.sys.country);
+        var name = Convert.ToString(dynamic?.name);
+        var temp = Convert.ToDecimal(dynamic?.main.temp);
+        var description = Convert.ToString(dynamic?.weather[0].description);
+        return (new Weather(country, name, temp, description), dynamic?.cod);
+    }
+}
